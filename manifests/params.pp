ebcb22c791c9c0cc032631ba0b5dba46920836cf
Frank Van Damme <frank.van.damme@vub.be>
2023-02-28 10:45:40 +0100

    refactor legacy variables

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,28 +3,28 @@ class poppins::params {
     $logdir               = "/root/poppins.d/logs"
     $configdir            = "/root/poppins.d/config"
     $zfs                  = "backups/poppins/server"
-    $included             =  { 
-	"/"         => "root",
-	"/var"      => "var",
-	"/boot"     => "boot",
-	"/home"     => "home",
+    $included             =  {
+        "/"         => "root",
+        "/var"      => "var",
+        "/boot"     => "boot",
+        "/home"     => "home",
     }
     $excluded             =  { "/" => "tmp" }
-    $hostdir_name         = $::hostname
+    $hostdir_name         = $facts[networking][hostname]
     $remote_ssh_port      = 22
-    $remote_host          = "$::ipaddress"
+    $remote_host          = $facts[networking][ip]
     $remote_user          = 'root'
     $remote_retry_count   = 0
     $remote_retry_timeout = 10
     $rsync_retry_count    = 1
     $rsync_retry_timeout  = 10
     $snapshots = {
-    	"incremental" => 4,
-    	"1-daily"     => 7,
-    	"1-weekly"    => 4,
-    	"1-monthly"   => 6,
-    	"6-monthly"   => 2,
-	"1-yearly"    => 1,
+        "incremental" => 4,
+        "1-daily"     => 7,
+        "1-weekly"    => 4,
+        "1-monthly"   => 6,
+        "6-monthly"   => 2,
+        "1-yearly"    => 1,
     }
 
     $ensure               = present
